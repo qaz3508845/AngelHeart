3076addce7b1961944b1a5fc05e01e82bedd6722
Dent <s16113220@stu.edu.tw>
2018-01-14 12:14:08 +0800

    123

--- app/src/main/java/com/example/voiceActivity.java
+++ app/src/main/java/com/example/voiceActivity.java
@@ -9,7 +9,7 @@ public class voiceActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_voice);
-        //ssss
+
 
 
 
