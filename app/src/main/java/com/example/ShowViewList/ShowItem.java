05e8043eccc28267f17e6d322edbce67229d7268
Dent <s16113220@stu.edu.tw>
2018-01-20 17:42:51 +0800

    修正小錯誤

--- app/src/main/java/com/example/ShowViewList/ShowItem.java
+++ app/src/main/java/com/example/ShowViewList/ShowItem.java
@@ -5,40 +5,67 @@ package com.example.ShowViewList;
  */
 
 public class ShowItem {
-    int id;
-    String account;
-    String password;
-    String phone;
+    int p_id;
+    String p_name;
+    String p_phone;
+    String p_account;
+    String p_password;
+    String p_address;
+    String p_datetime;
 
-    public int getId() {
-        return id;
+    public int getP_id() {
+        return p_id;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public void setP_id(int p_id) {
+        this.p_id = p_id;
     }
 
-    public String getAccount() {
-        return account;
+    public String getP_name() {
+        return p_name;
     }
 
-    public void setAccount(String account) {
-        this.account = account;
+    public void setP_name(String p_name) {
+        this.p_name = p_name;
     }
 
-    public String getPassword() {
-        return password;
+    public String getP_phone() {
+        return p_phone;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
+    public void setP_phone(String p_phone) {
+        this.p_phone = p_phone;
     }
 
-    public String getPhone() {
-        return phone;
+    public String getP_account() {
+        return p_account;
     }
 
-    public void setPhone(String phone) {
-        this.phone = phone;
+    public void setP_account(String p_account) {
+        this.p_account = p_account;
+    }
+
+    public String getP_password() {
+        return p_password;
+    }
+
+    public void setP_password(String p_password) {
+        this.p_password = p_password;
+    }
+
+    public String getP_address() {
+        return p_address;
+    }
+
+    public void setP_address(String p_address) {
+        this.p_address = p_address;
+    }
+
+    public String getP_datetime() {
+        return p_datetime;
+    }
+
+    public void setP_datetime(String p_datetime) {
+        this.p_datetime = p_datetime;
     }
 }
