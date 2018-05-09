ab9bc796fbfb3498c7d7b1f46c5953932eb80553
Dent <s16113220@stu.edu.tw>
2018-05-09 22:28:12 +0800

    進行整合

--- app/src/main/java/com/example/AlertDialogActivity.java
+++ app/src/main/java/com/example/AlertDialogActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class AlertDialogActivity extends AppCompatActivity {
-
+//AVX
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
